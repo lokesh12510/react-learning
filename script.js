6d8091ac554a0ea29520c5fb7da47e03c6f6ab68
lokeshk <lokesh.krishnamoorthy@intergy.com.au>
2025-01-27 22:26:07 +0530

    createElement

---
+++ script.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * React.createElement() is a function that create objects that describe the DOM nodes.
+ *
+ * ReactDOM.render() is a function that takes the object created by React.createElement() and renders it to the DOM.
+ *
+ *
+ */
+
+// const root = document.getElementById('root');
+
+// const h1 = document.createElement('h1');
+// h1.textContent = 'Hello, React!';
+
+// root.appendChild(h1);/
+
+const element = React.createElement("div", {}, [
+	React.createElement(
+		"h1",
+		{
+			id: "heading",
+		},
+		"Hello, React!"
+	),
+	React.createElement("p", null, "My new react project"),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
